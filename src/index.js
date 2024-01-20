fcc07e115cc16e85f1d569b7498aa35b9c4424ce
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2024-01-20 15:40:42 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,44 +21,20 @@ if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
 } else server = createHttpServer();
 
 server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
+  if (bare.shouldRoute(req)) bare.routeRequest(req, res);
+  else {
     if (req.url.startsWith("/data")) {
-      // Handle "/data" route (assuming analytics is defined)
       analytics(req, res, data);
     } else {
-      // Serve static files or handle 404 error
       serve(req, res, (err) => {
-        if (err) {
-          if (err.statusCode === 404) {
-            // Custom 404 page handling
-            return handle404(req, res);
-          } else {
-            // Other errors, respond with a generic error message
-            res.writeHead(err.statusCode, null, {
-              "Content-Type": "text/plain",
-            });
-            res.end("Error");
-          }
-        }
-        // Continue with normal flow if no error
+        res.writeHead(err?.statusCode || 500, null, {
+          "Content-Type": "text/plain",
+        });
+        res.end("Error");
       });
     }
   }
 });
-
-// Custom 404 page handling middleware
-const handle404 = (req, res) => {
-  res.writeHead(404, { "Content-Type": "text/html" });
-
-  // Read and send the custom 404 page
-  const custom404PagePath = fileURLToPath(new URL("../static/public/404.html", import.meta.url));
-  const custom404Page = readFileSync(custom404PagePath, "utf8");
-  res.end(custom404Page);
-};
-
-
 
 server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head);
